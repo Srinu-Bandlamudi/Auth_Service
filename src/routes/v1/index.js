b34ae46f7d76d319767abbff76e4d1a7e2059a00
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-10 15:12:01 +0530

    Added authrequest middleware to signin and singup

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,20 @@
 const express=require('express');
 
+const { AuthRequestValidators }=require('../../middlewares/index');
+
 const router=express.Router();
 
 const UserController=require('../../controllers/user-controller');
 
-router.post('/signup',UserController.create);
-router.post('/signin',UserController.signIn);
+router.post(
+    '/signup',
+    AuthRequestValidators.validateUserAuth,
+    UserController.create
+    );
+router.post(
+    '/signin',
+    AuthRequestValidators.validateUserAuth,
+    UserController.signIn
+    );
 
 module.exports=router;
