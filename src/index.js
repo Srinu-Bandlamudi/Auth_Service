b9eb7a5189386d340f8b4c3a1c154bd1ba9c7c25
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-10 12:21:56 +0530

    Added jwt create verify token logic

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ const bodyParser=require('body-parser');
 
 const { PORT }=require('./config/serverConfig');
 
-//const { User }=require('./models/index');
-//const bcrypt=require('bcrypt');
-const UserRepository=require('./repository/user-repository');
+const UserService=require('./services/user-service.');
 const app=express();
 
 const apiRoutes=require('./routes/index');
@@ -17,13 +15,12 @@ const StartServer=()=>{
     app.use('/api',apiRoutes);
     app.listen(PORT,async()=>{
         console.log(`Server started on port ${PORT}`);
-        const repo=new UserRepository();
-        const response=await repo.getById(1);
+        const service=new UserService();
+         //const newToken=service.createToken({email:'srinu55@gmail.com',id:1});
+        // console.log(newToken);
+         const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNyaW51NTVAZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTcwNzU0NzY5MSwiZXhwIjoxNzA3NTQ3NjkxfQ.1bh0bTusxUQDZX5rO7KooKsoI3CRV8JGIIYH8-6MuAI';
+        const response=service.verifyToken(token);
         console.log(response);
-        // const incomingPassword='123456';
-        // const user=await User.findByPk(3);
-        // const response=bcrypt.compareSync(incomingPassword,user.password);
-        // console.log(response);
 
     });
 }
--- src/services/user-service..js
+++ src/services/user-service..js
@@ -1,5 +1,8 @@
 const UserRepository=require('../repository/user-repository');
 
+const jwt=require('jsonwebtoken');
+const { JWT_KEY }=require('../config/serverConfig');
+
 class UserService{
     constructor(){
         this.userRepository=new UserRepository();
@@ -12,6 +15,24 @@ class UserService{
         } catch (error) {
             console.log('Something went wrong in the Service layer');
             throw error;
+        }
+    }
+    createToken(user){
+        try {
+            const result=jwt.sign(user,JWT_KEY,{expiresIn:'1d'});
+            return result;
+        } catch (error) {
+            console.log('Something went wrong in the Service layer');
+            throw error;
+        }
+    }
+    verifyToken(token){
+        try {
+            const response=jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log('Something went wrong in the Service layer',error);
+            throw error;   
         }
     }
 }
