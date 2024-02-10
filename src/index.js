be6ad8a1ea5df5abf1ff3310e4f1eb2a20390203
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-10 11:58:16 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { PORT }=require('./config/serverConfig');
 
 //const { User }=require('./models/index');
 //const bcrypt=require('bcrypt');
-
+const UserRepository=require('./repository/user-repository');
 const app=express();
 
 const apiRoutes=require('./routes/index');
@@ -17,6 +17,9 @@ const StartServer=()=>{
     app.use('/api',apiRoutes);
     app.listen(PORT,async()=>{
         console.log(`Server started on port ${PORT}`);
+        const repo=new UserRepository();
+        const response=await repo.getById(1);
+        console.log(response);
         // const incomingPassword='123456';
         // const user=await User.findByPk(3);
         // const response=bcrypt.compareSync(incomingPassword,user.password);
